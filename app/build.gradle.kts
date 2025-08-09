6168e956438becd9c70516d18efcd03b0340b888
Nitja Berakovic <nberakovic@gmail.com>
2025-08-09 14:48:53 +0200

    feat: integrate Hilt DI with KSP support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.ktlint)
     alias(libs.plugins.detekt)
     alias(libs.plugins.compose.compiler)
@@ -72,5 +74,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
     detektPlugins(libs.detekt.compose)
 }
