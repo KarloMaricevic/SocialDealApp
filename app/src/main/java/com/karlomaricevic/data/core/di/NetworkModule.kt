79b3b723ddc64a5092e25067114d6f986fb420aa
Nitja Berakovic <nberakovic@gmail.com>
2025-08-10 23:13:08 +0200

    refactor: rename package for consistency

--- app/src/main/java/com/karlomaricevic/data/core/di/NetworkModule.kt
+++ app/src/main/java/com/karlomaricevic/data/core/di/NetworkModule.kt
@@ -1,4 +1,4 @@
-package com.karlomaricevic.data.di
+package com.karlomaricevic.data.core.di
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
