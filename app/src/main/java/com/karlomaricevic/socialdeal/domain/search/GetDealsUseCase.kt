d2bb973d86267062a828e31f6e96267106db58b1
Nitja Berakovic <nberakovic@gmail.com>
2025-08-11 12:37:08 +0200

    feat: fetch deals including favorite info

--- app/src/main/java/com/karlomaricevic/socialdeal/domain/search/GetDealsUseCase.kt
+++ app/src/main/java/com/karlomaricevic/socialdeal/domain/search/GetDealsUseCase.kt
@@ -1,17 +1,22 @@
 package com.karlomaricevic.socialdeal.domain.search
 
 import com.karlomaricevic.socialdeal.data.deals.DealsRepository
+import com.karlomaricevic.socialdeal.data.favorites.FavoritesRepository
 import com.karlomaricevic.socialdeal.domain.core.di.IoDispatcher
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 
 class GetDealsUseCase @Inject constructor(
+    private val favoritesRepository: FavoritesRepository,
     private val dealsRepository: DealsRepository,
     @IoDispatcher private val dispatcher: CoroutineDispatcher,
 ) {
 
     suspend operator fun invoke() = withContext(dispatcher) {
-        dealsRepository.getDeals()
+        val favoritesIds = favoritesRepository.getFavoritesIds()
+        dealsRepository.getDeals().map { deals ->
+            deals.map { deal -> deal.copy(isFavorite = favoritesIds.contains(deal.id)) }
+        }
     }
 }
