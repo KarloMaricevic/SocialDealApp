3b5135ea0203eac2e6957b39f61a7ee4d86b04ab
Nitja Berakovic <nberakovic@gmail.com>
2025-08-13 22:28:11 +0200

    feat: make search screen change currency when user changes its preference
    + some minor clean up

--- app/src/main/java/com/karlomaricevic/socialdeal/feature/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/karlomaricevic/socialdeal/feature/search/viewmodel/SearchViewModel.kt
@@ -25,7 +25,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
